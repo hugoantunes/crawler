72d0bb43fac9376ed1d0eaa379a4e075119f2bf3
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2016-09-24 14:57:04 -0300

    check if url exist

--- src/crawler.py
+++ src/crawler.py
@@ -1,5 +1,14 @@
 # encoding: utf-8
+import requests
 
 
 class Crawler(object):
-    pass
+
+    def __init__(self, url):
+        self.url = url
+        self.check_status_code()
+
+    def check_status_code(self):
+        response = requests.get(self.url)
+        if response.status_code == 404:
+            raise Exception("Url does not exist")
--- src/test.py
+++ src/test.py
@@ -1,10 +1,19 @@
 import unittest
+import requests_mock
+
+from crawler import Crawler
 
 
 class TestCrawler(unittest.TestCase):
 
-    def test_(self):
-        assert True
+    def setUp(self):
+        self.url = "http://tests.com"
+
+    @requests_mock.mock()
+    def test_requested_url_should_raise_exception_when_status_code_is_404(self, mock):
+        mock.get(self.url, status_code=404)
+        with self.assertRaises(Exception):
+            Crawler(self.url)
 
 if __name__ == '__main__':
     unittest.main()
